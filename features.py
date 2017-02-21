cfeae34c052b5ed1e52213a19498e502bb934eb3
preethirag <preethi.raghavanms@gmail.com>
2017-02-21 13:40:12 -0800

    ada boost - Preethi

--- features.py
+++ features.py
@@ -8,6 +8,7 @@ from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 
 import date_transformer
 import imputing_transformer
@@ -55,4 +56,42 @@ def get_basic_features(df, ordinal, categorical,date_manip ,cont):
                                                     ('one_hot',one_hot)])),
                         ('cleanup',cleanup_transformer.cleanup_transformer())])
 
-    return features.transform(df)
+    return features, features.transform(df)
+
+#features with date, category manipulation without feature importance
+def get_poly_features(df, ordinal, categorical,date_manip ,cont):
+    date_pip = Pipeline([('extract',column_extractor.column_extractor(date_manip)),
+                ('date_manip', date_transformer.date_transformer('%Y-%m-%d')),
+                ('d-m-y-q-dow', FeatureUnion([('day',date_transformer.day_of_month_transformer()),
+                                            ('month',date_transformer.month_transformer()),
+                                            ('dow',date_transformer.day_of_week_transformer()),
+                                            ('quarter',date_transformer.month_quarter_transformer()),
+                                            ('year',date_transformer.year_transformer())])),
+                ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent')))])
+
+    continuous = Pipeline([
+            ('extract', column_extractor.column_extractor(cont)),
+            ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent'))),
+            ('scale', Normalizer())])
+
+    ordinal_pip = Pipeline([('extract', column_extractor.column_extractor(ordinal)),
+                            ('ord', categorical_transformer.ordinal_transformer(ordinal)),
+                           ('impute',imputing_transformer.imputing_transformer(Imputer(strategy='most_frequent')))])
+
+    one_hot = Pipeline([('extract', column_extractor.column_extractor(categorical)),
+                        ('lab_enc', categorical_transformer.label_transformer()),
+                        ('one_hot', ModelTransformer.ModelTransformer(OneHotEncoder(sparse=False)))])
+
+    select_features = Pipeline([('feat_imp',FeatureUnion([#('xtra_tree', SelectFromModel(ExtraTreesClassifier())),
+                                                      ('lin',SelectFromModel(LinearSVC(penalty="l1", dual=False)))]))])
+
+
+    features = Pipeline([('parallel', FeatureUnion([('date',date_pip),
+                                                    ('continuous',continuous),
+                                                    ('ordinal_pip',ordinal_pip),
+                                                    ('one_hot',one_hot)])),
+                        ('poly',ModelTransformer.ModelTransformer(PolynomialFeatures(degree=2))),
+                        ('select_features',select_features),
+                        ('cleanup',cleanup_transformer.cleanup_transformer())])
+
+    return features, features.transform(df)
--- models.py
+++ models.py
@@ -10,7 +10,10 @@ from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
 
 import date_transformer
 import imputing_transformer
@@ -20,6 +23,7 @@ import cleanup_transformer
 import ModelTransformer
 import classification_model
 import pandas_column_utilities
+from classification_model import kfold_classification_model
 
 import pandas as pd
 import numpy as np
@@ -52,22 +56,22 @@ def run_knn(X_train, X_test, y_train, y_test):
 
 def run_randomforestclassifier(X_train, X_test, y_train, y_test):
     param_grid = [
-    {'n_estimators':[5,10,20,30,50,100],'criterion':['entropy','gini'],'max_depth':[None, 2,5,10],
-    'min_samples_split':[5,7,10,15], 'oob_score':[True, False]}]
+    {'n_estimators':[50],'criterion':['entropy','gini'],'max_depth':[None, 5,10],
+    'min_samples_split':[7,10,15], 'oob_score':[True, False]}]
     clf = GridSearchCV(RandomForestClassifier(), param_grid)
     clf.fit(X_train, y_train)
 
     print clf.best_params_
 
     mod2 = kfold_classification_model(clf)
+    # mod2.fit(X_train, y_train)
     mod2.score(X_test, y_test)
 
 #work in progress
 def run_adaboost(X_train, X_test, y_train, y_test):
     param_grid = [
-    {'n_estimators':[5,10,20,30,50,100],'criterion':['entropy','gini'],'max_depth':[None, 2,5,10],
-    'min_samples_split':[5,7,10,15], 'oob_score':[True, False]}]
-    clf = GridSearchCV(RandomForestClassifier(), param_grid)
+    {'base_estimator':[DecisionTreeClassifier()],'n_estimators':[20,30,50],'learning_rate':[1, 0.5,2,5]}]
+    clf = GridSearchCV(AdaBoostClassifier(), param_grid)
     clf.fit(X_train, y_train)
 
     print clf.best_params_
